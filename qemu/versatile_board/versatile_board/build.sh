9ee8d05a47ebf2bedf07b05a2ffbbac8d4d711b9
czou <cao.zou@windriver.com>
2015-11-30 17:49:28 +0800

    add one board for u-boot
    Signed-off-by: czou <cao.zou@windriver.com>

--- qemu/versatile_board/versatile_board/build.sh
+++ qemu/versatile_board/versatile_board/build.sh
@@ -1,8 +1,9 @@
-make -C ~/github/linux-stable/ ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- O=~/cach/versatile_board_out vexpress_defconfig
-make -C ~/github/linux-stable/ ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- O=~/cach/versatile_board_out -j12
+#make -C ~/github/linux-stable/ ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=~/cach/versatile_board_out vexpress_defconfig
+#make -C ~/github/linux-stable/ ARCH=arm CROSS_COMPILE=arm-linux-gnueabi- O=~/cach/versatile_board_out -j12
 qemu-system-arm -M vexpress-a15 -nographic -kernel  ~/cach/versatile_board_out/arch/arm/boot/zImage -dtb ~/cach/versatile_board_out/arch/arm/boot/dts/vexpress-v2p-ca15_a7.dtb  -initrd initramfs2  -serial stdio --append "console=ttyAMA0 root=/dev/ram rdinit=/sbin/init"
 #qemu-system-arm -M vexpress-a15 -kernel  ~/cach/versatile_board_out/arch/arm/boot/zImage -dtb ~/cach/versatile_board_out/arch/arm/boot/dts/vexpress-v2p-ca15_a7.dtb  -serial stdio --append "console=ttyAMA0"
 
+exit
 qemu-system-arm  -nographic -M vexpress-a15 -kernel  ~/cach/versatile_board_out/arch/arm/boot/zImage -dtb ~/cach/versatile_board_out/arch/arm/boot/dts/vexpress-v2p-ca15_a7.dtb  -initrd initramfs2  --append "console=ttyAMA0 root=/dev/ram rdinit=/sbin/init"
 
 dd if=/dev/zero of=rootfs.img bs=1M count=20
@@ -25,3 +26,8 @@ cp ./linux-stable/arch/x86/boot/bzImage
 
 #启动qemu,同时装载刚才生成的根文件系统及内核文件
 qemu -hda ./rootfs.img -kernel ./bzImage -append "root=/dev/sda"
+
+
+#u-boot
+#make vexpress_ca9x4_defconfig
+#qemu-system-arm -M vexpress-a9  -m 128M -serial stdio -kernel u-boot -display none
